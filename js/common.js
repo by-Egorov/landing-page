bf99e0d7918244ff4ebf36c8684420440af20edd
Alexander <egorov.dev@gmail.com>
2022-08-05 02:39:34 +0300

    added slider(owl2)

--- js/common.js
+++ js/common.js
@@ -1,8 +1,14 @@
-$(".single-item").slick({
-	dots: true,
-  infinite: true,
-  speed: 500,
-  fade: true,
-  slide: "div",
-  cssEase: "linear"
-});
+const owl = $('.owl-carousel')
+owl.owlCarousel({
+	center: true,
+	loop: true,
+	margin: 30,
+	// startPosition: 1, // С какого слайда начинается
+	items: 3,
+})
+$('.slider__btn-prev').click(function () {
+	owl.trigger('prev.owl.carousel')
+})
+$('.slider__btn-next').click(function () {
+	owl.trigger('next.owl.carousel')
+})
