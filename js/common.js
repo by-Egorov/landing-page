0edb4edb8e482d92f8f9d54098ec541b80a1c491
Alexander <egorov.dev@gmail.com>
2022-08-08 00:35:38 +0300

    added mobile version

--- js/common.js
+++ js/common.js
@@ -4,11 +4,27 @@ owl.owlCarousel({
 	loop: true,
 	margin: 30,
 	startPosition: 1, // С какого слайда начинается
-	items: 3
+	items: 3,
+	responsiveClass: true,
+	responsive: {
+		576: {
+			items: 2,
+			center: false
+		}
+	}
 })
 $('.slider__btn-prev').click(function () {
 	owl.trigger('prev.owl.carousel')
 })
 $('.slider__btn-next').click(function () {
 	owl.trigger('next.owl.carousel')
+})
+
+
+const headerBurger = document.querySelector('.header__burger')
+const headerMenu = document.querySelector('.header__menu')
+
+headerBurger.addEventListener('click', function () {
+	headerBurger.classList.toggle('active')
+	headerMenu.classList.toggle('active')
 })
